f2e3b8924b96c99625024b5180f0959bd5ada4ae
bastianharren <bastian@harren.co>
2015-12-02 11:52:08 +0100

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head><title>Hello World</title></head>
+  <body>
+    <p><h1>Hello World!</h1></p>
+    <p>
+      <?php
+        echo date("Y-m-d");
+      ?>
+    </p>
+  </body>
+</html>
